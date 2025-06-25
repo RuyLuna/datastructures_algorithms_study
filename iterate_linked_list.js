744ecd7e5ac9450ac2a47a3a86c7f6c1bd636d74
RuyLuna <135095052+RuyLuna@users.noreply.github.com>
2025-06-25 15:04:40 -0700

    Create iterate_linked_list.js
    Simple file that creates a linked list, and then iterates through it

---
+++ iterate_linked_list.js
@@ -0,0 +1,24 @@
+// Declare the node class
+class Node {
+  constructor(data){
+    this.data = data;
+    this.next = null;
+  }
+}
+
+// Create the nodes for the linked list
+let node1 = new Node(1);
+let node2 = new Node(2);
+let node3 = new Node(3);
+let node4 = new Node(4);
+
+node1.next = node2;
+node2.next = node3;
+node3.next = node4;
+
+// Iterate through the linked list
+currentNode = node1;
+while(currentNode){
+  console.log("currentNode: ",currentNode);
+  currentNode = currentNode.next;
+}
